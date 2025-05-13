855b441f85d0da09ee03249827e66a2897531370
AffanWajid12 <affanwajidali@gmail.com>
2025-05-13 12:08:18 +0500

    first commit

---
+++ product-service/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3002;
+
+app.get('/products', (req, res) => {
+    res.json([
+        { id: 101, name: "Laptop" },
+        { id: 102, name: "Phone" }
+    ]);
+});
+
+app.listen(port, () => {
+    console.log(`Product service running on port ${port}`);
+});
